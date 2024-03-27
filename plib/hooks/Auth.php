bcae312bf2d747253bf382e98eada1a7571bfed8
Silentassassin <admin@jupiterservers.com>
2024-03-26 22:31:12 -0400

    test

--- plib/hooks/Auth.php
+++ plib/hooks/Auth.php
@@ -13,17 +13,15 @@ class Modules_LdapAuth_Auth extends pm_Hook_Auth
             $port = "636";
         }
 
-        $test = @ldap_connect($protocol . pm_Settings::get('host'), $port);
-        \pm_Log::info("LDAP connection test: " . $test);
-
-        curl_setopt($ch, CURLOPT_URL, $protocol . pm_Settings::get('host') . ":" . $port . "/DC=domaingenie,DC=net?attributes?sub?(objectClass=inetorgperson)");
+        curl_setopt($ch, CURLOPT_URL, "https://security.domaingenie.net/realms/domaingenie/protocol/openid-connect/token");
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
         // Force closing TLS channel after login
         if (!\pm_ProductInfo::isWindows()) {
             curl_setopt($ch, CURLOPT_CONNECT_ONLY, true);
         }
-        curl_setopt($ch, CURLOPT_USERPWD, pm_Settings::get('loginPrefix') . $login . pm_Settings::get('loginSuffix') . ":" . $password);
+        curl_setopt($ch, CURLOPT_USERPWD, $login . ":" . $password);
         \pm_Log::debug('Sending a request to the LDAP server');
         $result = curl_exec($ch);
         if ($result === false) {
