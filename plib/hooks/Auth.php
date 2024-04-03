9233852861e53805f9fe65e22852fac293d55b56
Silentassassin <admin@jupiterservers.com>
2024-04-03 12:31:53 -0400

    Test

--- plib/hooks/Auth.php
+++ plib/hooks/Auth.php
@@ -6,12 +6,14 @@ class Modules_LdapAuth_Auth extends pm_Hook_Auth
     public function auth($login, $password)
     {
         \pm_Log::info("Authenticating user $login");
+        \pm_Log::error("Authenticating user $login");
+        \pm_Log::debug("Authenticating user $login");
         return true;
     }
 
     public function isEnabled()
     {
-        return (bool)pm_Settings::get('enable');
+        return true;
     }
 
     public function breakChainOnFailure()
